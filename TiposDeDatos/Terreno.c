831f7d0673dc65ff6e668cad54ea863f59554d6c
LMendoza70 <luis.mendoza@uthh.edu.mx>
2024-09-11 09:47:25 -0600

    Clase Tipos de datos y expreciones

---
+++ TiposDeDatos/Terreno.c
@@ -0,0 +1,28 @@
+//en este apatardado agregamos las librerias para manejo de entradas y salidas
+#include<stdio.h>
+/*esta libreria es un poco antigua y actualmente casi no se utiliza
+esto es un comentario multilinea*/
+#include<conio.h>
+
+int main()
+{
+    //entradas
+    float frente=0,preciom2=0,preciofinal=0;
+    float fondo=0, area=0;
+    //asignar valores a las variables
+    printf("Ingresa cuantos metros tiene de frente el terreno\n");
+    scanf("%f", &frente);
+    printf("Ingresa Cuanto metros tiene el fondo del terreno\n");
+    scanf("%f", &fondo);
+    printf("Cual es el costo del metro cuadrado en esta zona?\n");
+    scanf("%f", &preciom2);
+    //proceso 
+    area=frente*fondo;
+    preciofinal=area*preciom2;
+    //salida
+    printf("El area del terreno es : %f metros cuadrados\nsu costo seria de %f pesos",area,preciofinal);
+    getch();
+
+    return 0;
+    
+}
